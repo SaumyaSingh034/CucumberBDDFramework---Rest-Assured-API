d71a7cb5d9950230f94313108ab5d96ef02378f5
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-05 16:30:56 +0530

    Initial Commit

--- src/main/java/utilities/RequestSpecificationBuilder.java
+++ src/main/java/utilities/RequestSpecificationBuilder.java
@@ -2,17 +2,29 @@ package utilities;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class RequestSpecificationBuilder {
     RequestSpecification requestSpecification;
 
     public RequestSpecification createRequestSpecification(){
         RestAssured.baseURI="https://rahulshettyacademy.com";
-        requestSpecification =new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
-                .addFilter(Re)
-                .setContentType(ContentType.JSON).build();
+        try {
+            PrintStream log = new PrintStream(new FileOutputStream("looging.txt"));
+            requestSpecification =new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
+                    .addFilter(RequestLoggingFilter.logRequestTo(log))
+                    .setContentType(ContentType.JSON).build();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         return requestSpecification;
     }
 }
